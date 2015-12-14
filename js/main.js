2570f84187631d0e6f3ddad249da1cda7fd6ab43
galdinorosas <dino.rosas@gmail.com>
2015-12-13 21:47:56 -0800

    made some animations using velocityJS to the header title and to have the nav links scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+
+	console.log("hello");
+	$("h1").velocity("transition.slideLeftIn", { duration: 2100 , delay: 500});
+
+	$("li a").on("click", function(){
+
+		// console.log($(".topLink").attr("href"));
+		var href = $(this).attr("href");
+
+		$(href).velocity("scroll", {duration: 1500});
+
+	});
 
 
-console.log("hello");
+
+	
+
+
+
+});
+
+
